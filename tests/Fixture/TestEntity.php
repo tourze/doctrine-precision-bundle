c9c479cd6cbd463a525cf5b3827c0ebb9fe33e76
github-ci <you@example.com>
2025-06-18 18:57:35 +0800

    fix tests

--- tests/Fixture/TestEntity.php
+++ tests/Fixture/TestEntity.php
@@ -2,27 +2,34 @@
 
 namespace Tourze\DoctrinePrecisionBundle\Tests\Fixture;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Tourze\DoctrinePrecisionBundle\Attribute\PrecisionColumn;
 
 #[ORM\Entity]
-class TestEntity
+#[ORM\Table(name: 'test_entity', options: ['comment' => 'Test Entity for Precision Bundle'])]
+class TestEntity implements \Stringable
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
+    #[ORM\Column(options: ['comment' => 'ID'])]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'decimal', precision: 10)]
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, options: ['comment' => '价格'])]
     #[PrecisionColumn]
     private ?string $price = null;
 
-    #[ORM\Column(type: 'decimal', precision: 8, scale: 4)]
+    #[ORM\Column(type: Types::DECIMAL, precision: 8, scale: 4, options: ['comment' => '固定精度价格'])]
     private ?string $fixedPrecisionPrice = null;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function __toString(): string
+    {
+        return 'TestEntity #' . ($this->id ?? 'new');
     }
 
     public function getPrice(): ?string
